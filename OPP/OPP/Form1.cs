513e1621efcb642ac9b0eee7be3f6f17c08a6c5f
nangbx <61286902+nangbx@users.noreply.github.com>
2021-04-12 23:32:35 +0700

    Add files via upload

---
+++ OPP/OPP/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace OPP
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hwnd, int wmsg, int wparam, int lparam);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Resize nav
+        private void btnMenu_Click(object sender, EventArgs e)
+        {
+            if (pnMenuVertical.Width == 250)
+            {
+                pnMenuVertical.Width = 70;
+            }
+            else
+                pnMenuVertical.Width = 250;
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        
+
+        private void btnMin_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void btnResize_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+            btnMax.Visible = true;
+            btnResize.Visible = false;
+        }
+
+        private void btnMax_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+            btnResize.Visible = true;
+            btnMax.Visible = false;
+        }
+        private void addFrm(object frmInfo)
+        {
+            if (this.pnContent.Controls.Count > 0)
+                this.pnContent.Controls.RemoveAt(0);
+            Form form = frmInfo as Form;
+            form.TopLevel = false;
+            form.Dock = DockStyle.Fill;
+            this.pnContent.Controls.Add(form);
+            form.Show();
+        }
+        private void btnStatistical_Click(object sender, EventArgs e)
+        {
+            addFrm(new frmStatistical());
+        }
+
+        // Nắm vào vùng title di chuyển app
+        private void pnTitle_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+    }
+}
---
+++ OPP/OPP/frmLogin.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Data.SqlClient;
+
+namespace OPP
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+            txtPass.UseSystemPasswordChar = false;
+            
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void txtUserName_Click(object sender, EventArgs e)
+        {
+            txtUserName.BackColor = Color.White;
+            panel3.BackColor = Color.White;
+            panel4.BackColor = SystemColors.Control;
+            txtPass.BackColor = SystemColors.Control;
+        }
+
+        private void txtPass_Click(object sender, EventArgs e)
+        {
+            txtPass.BackColor = Color.White;
+            panel4.BackColor = Color.White;
+            txtUserName.BackColor = SystemColors.Control;
+            panel3.BackColor = SystemColors.Control;
+        }
+
+        private void pictureBox3_MouseDown(object sender, MouseEventArgs e)
+        {
+            txtPass.UseSystemPasswordChar = false;
+        }
+
+        private void pictureBox3_MouseUp(object sender, MouseEventArgs e)
+        {
+            txtPass.UseSystemPasswordChar = true;
+        }
+
+        private void txtPass_Enter(object sender, EventArgs e)
+        {
+            txtPass.BackColor = Color.White;
+            panel4.BackColor = Color.White;
+            txtUserName.BackColor = SystemColors.Control;
+            panel3.BackColor = SystemColors.Control;
+            if(txtPass.Text == "Password")
+            {
+                txtPass.Text = "";
+                txtPass.UseSystemPasswordChar = true;
+            }    
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            SqlConnection sqlConnection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\nangb\Desktop\OOP\OPP\OPP\Database1.mdf;Integrated Security=True");
+            string query = "Select * from dbo.taiKhoan where username = '" + txtUserName.Text + "' and password = '" + txtPass.Text + "'";
+            SqlDataAdapter sda = new SqlDataAdapter(query, sqlConnection);
+            DataTable data = new DataTable();
+            sda.Fill(data);
+            if(data.Rows.Count == 1)
+            {
+                this.Hide();
+                Form1 form1 = new Form1();
+                form1.Show();
+                    
+            }
+            else
+            {
+                MessageBox.Show("Check your username and password", "Thông báo", MessageBoxButtons.OK);
+                this.Show();
+            }    
+
+
+        }
+
+        private void txtUserName_Enter(object sender, EventArgs e)
+        {
+            if(txtUserName.Text == "User name")
+            {
+                txtUserName.Text = "";
+
+            }    
+        }
+
+        private void txtUserName_Leave(object sender, EventArgs e)
+        {
+            if(txtUserName.Text == "")
+            {
+                txtUserName.Text = "User name";
+            }    
+        }
+
+        private void txtPass_Leave(object sender, EventArgs e)
+        {
+            if(txtPass.Text == "")
+            {
+                txtPass.Text = "Password";
+                txtPass.UseSystemPasswordChar = false;
+            }    
+        }
+    }
+}
