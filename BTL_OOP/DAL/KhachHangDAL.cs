538afea79867bd0407502f06a663b4f25dc0b409
phammanhhung <phammanhhung10501@gmail.com>
2021-04-17 21:14:17 +0700

    Add KhachHangDAL

--- BTL_OOP/DAL/KhachHangDAL.cs
+++ BTL_OOP/DAL/KhachHangDAL.cs
@@ -18,6 +18,35 @@ namespace DAL
             _context.Load();
         }
 
+        public void SaveChanges() => _context.SaveChanges();
 
+        public KhachHangDTO TimKiemKhachHangTheoMaGianHang(string maGianHang)
+        {
+            return _context.DsKhachHang.First(khachHang => khachHang.MaGianHang == maGianHang);
+        }
+
+        public bool ThemKhachHang(KhachHangDTO dto)
+        {
+            _context.DsKhachHang.Add(dto);
+            return true;
+        }
+
+        public bool CapNhatKhachHang(string maGianHang, KhachHangDTO dto)
+        {
+            KhachHangDTO khachHangCapNhat = TimKiemKhachHangTheoMaGianHang(maGianHang);
+            if (khachHangCapNhat == null)
+                return false;
+            khachHangCapNhat = dto;
+            return true;
+        }
+
+        public bool XoaKhachHang(string maGianHang)
+        {
+            KhachHangDTO khachHang = TimKiemKhachHangTheoMaGianHang(maGianHang);
+            if (khachHang == null)
+                return false;
+            _context.DsKhachHang.Remove(khachHang);
+            return true;
+        }
     }
 }
