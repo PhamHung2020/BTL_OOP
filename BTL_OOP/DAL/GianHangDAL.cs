b00799bbb65dc3fe58f1cfde6f43e7e0597d7021
phammanhhung <phammanhhung10501@gmail.com>
2021-05-01 08:46:32 +0700

    Add DAL project

---
+++ BTL_OOP/DAL/GianHangDAL.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using DTO;
+
+namespace DAL
+{
+    public class GianHangDAL
+    {
+        private List<GianHangDTO> _dsGianHang;
+
+        public List<GianHangDTO> DsGianHang
+        {
+            get
+            {
+                if (_dsGianHang == null)
+                {
+                    Load();
+                }
+                return _dsGianHang;
+            }
+        }
+
+        public void Load()
+        {
+            _dsGianHang = new List<GianHangDTO>()
+            {
+                new GianHangCaoCapDTO("CC101", 5.6, "101", false, 5, 6),
+                new GianHangCaoCapDTO("CC203", 10.25, "202", false, 10, 12),
+                new GianHangTieuChuanDTO("TC101", 5.6, "101", true, "Ton", "Nhua"),
+                new GianHangTieuChuanDTO("TC202", 10.25, "202", false, "Sat", "Dong")
+            };
+        }
+
+        public List<T> LayDanhSachGianHang<T>() where T : GianHangDTO
+        {
+            if (typeof(T) == typeof(GianHangDTO))
+            {
+                return _dsGianHang as List<T>;
+            }
+
+            List<T> list = new List<T>();
+            foreach (GianHangDTO gianHang in _dsGianHang)
+            {
+                if (gianHang is T)
+                {
+                    list.Add(gianHang as T);
+                }
+            }
+            return list;
+        }
+    }
+}
---
+++ BTL_OOP/DAL/KhachHangDAL.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using DTO;
+
+namespace DAL
+{
+    public class KhachHangDAL
+    {
+        private List<KhachHangDTO> _dsKhachHang;
+
+        public List<KhachHangDTO> DsKhachHang
+        {
+            get
+            {
+                if (_dsKhachHang == null)
+                {
+                    Load();
+                }
+                return _dsKhachHang;
+            }
+        }
+
+        public void Load()
+        {
+            _dsKhachHang = new List<KhachHangDTO>()
+            {
+                new KhachHangDTO("00001", "Pham Hung", "Lang Son", "TC101", new DateTime(2021, 3, 15), new DateTime(2021, 4, 16), 10000000),
+            };
+        }
+    }
+}
