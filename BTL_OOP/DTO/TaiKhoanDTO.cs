ecdcbfc3c2980107c60ad1a9649b5e7243c17978
DuyTien <ddtien0909@gmail.com>
2021-05-01 11:18:28 +0700

    Add class TaiKhoan

---
+++ BTL_OOP/DTO/TaiKhoanDTO.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    class TaiKhoanDTO
+    {
+        private string _email;
+        public string Email
+        {
+            get => _email;
+            set { if (value != null) _email = value; }
+        }
+
+        private string _passWord;
+        public string PassWord
+        {
+            get => _passWord;
+            set { if (value != null) _passWord = value; }
+        }
+
+        public TaiKhoanDTO(string _email, string _passWord)
+        {
+            this._email = _email;
+            this._passWord = _passWord;
+        }
+    }
+}
